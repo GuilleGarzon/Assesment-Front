12b5193aee1aadac42d4d3e786ae047fe4a961fd
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-07-26 22:27:31 -0500

    feat: add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import ProductCard from '../components/ProductCard';
+
+function Home() {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/services/Products.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://fakestoreapi.com/';
+
+export async function getProducts() {
+  const response = await fetch(`${BASE_URL}products/`);
+  return response.json();
+}
+
+export async function getProduct(id) {
+  const response = await fetch(`${BASE_URL}products/${id}`);
+  return response.json();
+}
